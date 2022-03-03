5337240a9425ba9bdafbf80affe7490e2160fc8d
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2022-03-03 10:10:20 -0500

    Grant admin consent in application

--- infrastructure/azuread-oidc.tf
+++ infrastructure/azuread-oidc.tf
@@ -1,8 +1,43 @@
+resource "random_uuid" "oidc_user" {}
+resource "random_uuid" "oidc_admin" {}
+resource "random_uuid" "oidc_app_role" {}
+
 resource "azuread_application" "oidc" {
   display_name = "${azurerm_resource_group.resources.name}-oidc-auth"
   owners       = [data.azuread_client_config.current.object_id]
 
   group_membership_claims = ["All"]
+
+  api {
+    oauth2_permission_scope {
+      admin_consent_description  = "Allow the application to access Azure AD on behalf of the signed-in user."
+      admin_consent_display_name = "Access Azure AD"
+      enabled                    = true
+      id                         = random_uuid.oidc_user.result
+      type                       = "User"
+      user_consent_description   = "Allow the application to access AzureAD on your behalf."
+      user_consent_display_name  = "Access Azure AD"
+      value                      = "user_impersonation"
+    }
+
+    oauth2_permission_scope {
+      admin_consent_description  = "Administer the Boundary OIDC auth method application"
+      admin_consent_display_name = "Administer"
+      enabled                    = true
+      id                         = random_uuid.oidc_admin.result
+      type                       = "Admin"
+      value                      = "administer"
+    }
+  }
+
+  app_role {
+    allowed_member_types = ["User", "Application"]
+    description          = "Admins can manage roles and perform all task actions"
+    display_name         = "Admin"
+    enabled              = true
+    id                   = random_uuid.oidc_app_role.result
+    value                = "admin"
+  }
 
   required_resource_access {
     resource_app_id = "00000003-0000-0000-c000-000000000000" # Microsoft Graph
